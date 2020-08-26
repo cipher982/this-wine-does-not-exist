dc862ec07769a7c88ebb2839124030ec929984ce
David Rose <david010@gmail.com>
2020-08-25 20:36:27 -0500

    refactored scraping code to capture more data

--- scraperFuncs.py
+++ scraperFuncs.py
@@ -1,42 +1,34 @@
-import pandas as pd
-import numpy as np
-import glob
-from tqdm import tqdm
-from bs4 import BeautifulSoup
-from requests import get
-import lxml
+import random
+import re
+import requests
 
-def scrapeHTML(url):
+def GET_UA():
+    uastrings = ["Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",\
+                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36",\
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/600.1.25 (KHTML, like Gecko) Version/8.0 Safari/600.1.25",\
+                "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0",\
+                "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",\
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",\
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/600.1.17 (KHTML, like Gecko) Version/7.1 Safari/537.85.10",\
+                "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",\
+                "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0",\
+                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.104 Safari/537.36"\
+                ]
+ 
+    return random.choice(uastrings)
+
+def scrape_html(url):
     print(url)
+
+    headers = {'User-Agent': GET_UA()}
+
     try:
-        html = get("http://www.wine.com"+url).text
+        response = requests.get("http://www.wine.com"+url, headers=headers)
     except Exception as e:
-        html = e
-        return url, html
+        return url, e
+      
+    url_clean = re.sub('/', '∕',url)
+    with open(f'E:/wine_pages/{response.status_code}_{url_clean}.html','w', encoding="utf-8") as f:
+      f.write(response.text)
 
-    return url, html
-
-
-def extractDescription(html):
-    try:
-        soup = BeautifulSoup(html, "lxml")
-        desc = soup.find("div", {"itemprop":"description"})\
-                   .find("div", {"class":"viewMoreModule_text"}).text
-    except Exception as e:
-        desc = e
-
-    return desc
-
-def scrapeAndExtract(url):
-    print(url.encode("ascii", errors="ignore").decode())
-    try:
-        html = get("http://www.wine.com"+url).text
-        soup = BeautifulSoup(html, "lxml")
-        desc = soup.find("div", {"itemprop":"description"})\
-                   .find("div", {"class":"viewMoreModule_text"})\
-                   .encode("ascii", errors="ignore").decode()
-    except Exception as e:
-        desc = e
-        
-    url = url.encode("ascii", errors="ignore").decode()
-    return url, desc
+    return url, response.status_code
