e81d90cae11f39ed6454fdc65a3839b8d476f2a4
David Rose <david010@gmail.com>
2021-03-12 13:24:26 -0500

    clean up repo

--- misc/gpt2_finetune.ps1
+++
@@ -1,8 +0,0 @@
-python transformers/examples/language-modeling/run_language_modeling.py \
-    -output_dir gpt2_output \
-    -model_type gpt2 \
-    -model_name_or_path gpt2 \
-    -do_train \
-    -train_data_file "data/scraped/name_desc_nlp_ready_train.txt" \
-    -do_eval \
-    -eval_data_file "data/scraped/name_desc_nlp_ready_test.txt"
