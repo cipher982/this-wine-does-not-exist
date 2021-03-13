f817f5186caa7e82e4c808c2b2765ec0627437f0
David Rose <david010@gmail.com>
2021-03-13 12:01:11 -0500

    improve colab server scripts

--- colab/setup.sh
+++ colab/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 python3 -m pip install wandb pytorch_lightning deepspeed
+python3 -m pip install torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html
 
 git clone https://github.com/huggingface/transformers
 cd transformers
