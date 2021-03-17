29ce3b017d80027e340c07e94b849c87e4ab7f76
David-Rose <drose@zetaglobal.com>
2021-03-16 21:58:19 -0500

    update setup.sh for CUDA 11.2

--- colab/setup.sh
+++ colab/setup.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-apt-get purge nvidia*
-apt-get autoremove
+#apt-get purge nvidia*
+#apt-get autoremove
 
-wget https://developer.download.nvidia.com/compute/cuda/11.1.0/local_installers/cuda_11.1.0_455.23.05_linux.run
-cuda_11.1.0_455.23.05_linux.run
+#wget https://developer.download.nvidia.com/compute/cuda/11.1.0/local_installers/cuda_11.1.0_455.23.05_linux.run
+#cuda_11.1.0_455.23.05_linux.run
+apt-get update
+apt-get install cuda # gets cuda 11.2
 
 python3 -m pip install torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html
-python3 -m pip install wandb pytorch_lightning mpi4py deepspeed
+python3 -m pip install wandb pytorch_lightning deepspeed mpi4py
 
 git clone https://github.com/huggingface/transformers
 cd transformers
