c61a7b42f5ddccf15ad1892cace4c9a968c01731
root <david010@gmail.com>
2021-03-19 23:14:16 +0000

    fix arguments and add saving checkpoints

--- gpt2_deepspeed/deep_gpt2.py
+++ gpt2_deepspeed/deep_gpt2.py
@@ -8,17 +8,75 @@ import deepspeed
 import wandb
 
 os.environ['TOKENIZERS_PARALLELISM'] = "false"
+MODEL_TYPE = 'distilgpt2'
 
-MODEL_TYPE = 'gpt2-xl'
-
-# wandb setup
 api_key_path = "./.wandb/api_key"
 os.makedirs(os.path.dirname(api_key_path), exist_ok=True)
 with open(api_key_path, 'r') as f:
     wandb_api_key = f.read()
+
+def add_argument():
+    parser = argparse.ArgumentParser(description='gpt2-wine')
+
+    # cuda
+    parser.add_argument('--with_cuda',
+                        default=False,
+                        action='store_true',
+                        help='use CPU in case there\'s no GPU support')
+
+    # train
+    parser.add_argument('-b',
+                        '--batch_size',
+                        default=1,
+                        type=int,
+                        help='batch size (default: 1)')
+    parser.add_argument('-e',
+                        '--epochs',
+                        default=1,
+                        type=int,
+                        help='number of total epochs (default: 1)')
+    parser.add_argument('--local_rank',
+                        type=int,
+                        default=-1,
+                        help='local rank passed from distributed launcher')
+    parser.add_argument(
+        '--load_dir',
+        type=str,
+        default=None,
+        help='Directory to load checkpoint from')
+    parser.add_argument(
+        '--ckpt_id',
+        type=str,
+        default='',
+        help='Checkpoint ID to load for model')
+    parser.add_argument(
+        '--save_dir',
+        type=str,
+        default=f'./deep_{MODEL_TYPE}_ckpt',
+        help='Directory to save checkpoint to')
+    parser.add_argument(
+        '--save_interval',
+        type=int,
+        default=1000,
+        help='Step interval for saving checkpoints')
+
+    # Include DeepSpeed configuration arguments
+    parser = deepspeed.add_config_arguments(parser)
+    args = parser.parse_args()
+    return args
+args = add_argument()
+print("Load Dir: ", args.load_dir)
+print(f"Saving every {args.save_interval} steps")
+
+if args.load_dir is not None:
+    resume_training = True
+else:
+    resume_training = False
+
+
 print(f"WandB API Key: {wandb_api_key}")
-wandb.login(anonymous='never', key=wandb_api_key)
-wandb.init(project=f"wine_{MODEL_TYPE}_deepspeed")
+#wandb.login(anonymous='never', key=wandb_api_key)
+wandb.init(project=f"wine_{MODEL_TYPE}_deepspeed", resume=resume_training)
 
 # Setup PyTorch Dataset subclass
 class wineDataset(torch.utils.data.Dataset):
@@ -75,13 +133,17 @@ wine_dataset = wineDataset(wine_encodings)
 #wine_dataset_test = wineDataset(wine_encodings_test)
 print("Created PyTorch DataSet")
 
-data_loader = torch.utils.data.DataLoader(wine_dataset, num_workers=0)
+data_loader = torch.utils.data.DataLoader(
+    wine_dataset, 
+    num_workers=0, 
+    batch_size=args.batch_size
+)
 print("Created DataLoader")
 
 # Load model
 model_path = f'./{MODEL_TYPE}_model'
 if os.path.exists(model_path):
-    print(f"Found saved model at {model_path}, loading. . .")
+    print(f"Found cached model at {model_path}, loading. . .")
     model = torch.load(model_path)
     print("Loaded gpt2 model")
 else:
@@ -97,46 +159,10 @@ else:
     model.resize_token_embeddings(len(tokenizer))
     print("Resized token embeddings")
 
-    # Send to GPU, set train mode, and save
-    #model.to('cuda')
-    #model = model.train()
     torch.save(model, model_path)
     print(f"Saved model to {model_path}")
 
 print(f"Total parameters: {model.num_parameters()/1e6:.2f}M")
-
-def add_argument():
-
-    parser = argparse.ArgumentParser(description='gpt2-wine')
-
-    #data
-    # cuda
-    parser.add_argument('--with_cuda',
-                        default=False,
-                        action='store_true',
-                        help='use CPU in case there\'s no GPU support')
-
-    # train
-    parser.add_argument('-b',
-                        '--batch_size',
-                        default=1,
-                        type=int,
-                        help='mini-batch size (default: 32)')
-    parser.add_argument('-e',
-                        '--epochs',
-                        default=1,
-                        type=int,
-                        help='number of total epochs (default: 30)')
-    parser.add_argument('--local_rank',
-                        type=int,
-                        default=-1,
-                        help='local rank passed from distributed launcher')
-
-    # Include DeepSpeed configuration arguments
-    parser = deepspeed.add_config_arguments(parser)
-    args = parser.parse_args()
-    return args
-args = add_argument()
 
 parameters = filter(lambda p: p.requires_grad, model.parameters())
 
@@ -146,12 +172,22 @@ model_engine, optimizer, trainloader, _ = deepspeed.initialize(
     model_parameters=parameters,
 )
 
-for step, batch in enumerate(data_loader):
 
+if resume_training == True:
+    # Load checkpoint
+    _, client_sd = model_engine.load_checkpoint(args.load_dir, args.ckpt_id)
+    resume_step = client_sd['step']
+    print(f"Resuming training from step: {resume_step}")
+else:
+    client_sd = {}
+    resume_step = -1
+
+
+for step, batch in enumerate(data_loader, start=resume_step+1):
     # Forward
     batch['input_ids'] = batch['input_ids'].to('cuda')
-    batch['attention_mask'] = batch['attention_mask'][0,:].to('cuda')
-    batch['labels'] = batch['labels'][0,:].to('cuda')
+    batch['attention_mask'] = batch['attention_mask'][:,:].to('cuda')
+    batch['labels'] = batch['labels'][:,:].to('cuda')
     
     output = model_engine(
         input_ids=batch['input_ids'], 
@@ -172,6 +208,13 @@ for step, batch in enumerate(data_loader):
 
     # Weight update
     model_engine.step()
+
+    # Save checkpoint
+    if step % args.save_interval:
+        print(f"Saving checkpoint {step}")
+        client_sd['step'] = step
+        ckpt_id = f"step_{step}_loss_{loss.item()}"
+        model_engine.save_checkpoint(args.save_dir, ckpt_id, client_state=client_sd)
 
 
 print("YAYYY!!")
