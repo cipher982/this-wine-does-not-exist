baa6a4d8bcedd9ce8b2ef429705f6c0b1c337a8e
Ubuntu <david010@gmail.com>
2021-03-22 21:11:25 +0000

    update run paramters

--- gpt2_deepspeed/run.sh
+++ gpt2_deepspeed/run.sh
@@ -2,6 +2,7 @@
 
 python3 deep_gpt2.py --deepspeed_config ds_config_1gpu.json  \
 --save_dir /datadrive/runs \
---batch_size 3 \
---save_interval 1000
+--train_batch_size 2 \
+--save_interval 500 \
+--epochs 5
 #--load_dir "/datadrive/runs" --ckpt_id "step_63_loss_13.9765625" 
