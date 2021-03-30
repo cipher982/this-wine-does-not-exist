cb5704e13b51d050c0256be633ef2cf1d0975325
root <david010@gmail.com>
2021-03-30 22:18:52 +0000

    rename for clarification

--- gpt2_deepspeed/run.sh
+++ gpt2_deepspeed/run.sh
@@ -2,7 +2,7 @@
 
 python3 deep_gpt2.py --deepspeed_config ds_config_1gpu.json  \
 --save_dir /datadrive/runs \
---train_batch_size 2 \
---save_interval 500 \
---epochs 5
-#--load_dir "/datadrive/runs" --ckpt_id "step_63_loss_13.9765625" 
+--train_batch_size 5 \
+--save_interval 2000 \
+--epochs 5 
+#--load_dir "/datadrive/runs" --ckpt_id "step_8750" 
