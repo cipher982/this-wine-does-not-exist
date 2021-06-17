dbd3c47b515466e01adddd79b4eabbf78079485e
David Rose <david010@gmail.com>
2021-06-16 21:37:11 -0400

    format with Black

--- config.py
+++ config.py
@@ -7,12 +7,12 @@
 
 """Global configuration."""
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 # Paths.
 
-result_dir = 'results'
-data_dir = 'datasets'
-cache_dir = 'cache'
-run_dir_ignore = ['results', 'datasets', 'cache']
+result_dir = "results"
+data_dir = "datasets"
+cache_dir = "cache"
+run_dir_ignore = ["results", "datasets", "cache"]
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
